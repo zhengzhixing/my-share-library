1d623afec68cdd7dd60d3b3e7575ef69f089c7d3
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-05 14:56:13 +0800

    Update buildPlugin.groovy

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -2,6 +2,5 @@ def call(Map config) {
     node {
         git url: "git@github.com:zhengzhixing/${config.name}-life.git"
         sh 'mvn install'
-        mail to: '...', subject: "${config.name} plugin build", body: '...'
     }
 }
