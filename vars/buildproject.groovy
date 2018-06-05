261aae08e909cdb3bd6ceb3f8547b14f70b4f2e3
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-05 11:51:11 +0800

    Update buildproject.groovy

--- vars/buildproject.groovy
+++ vars/buildproject.groovy
@@ -1,4 +1,5 @@
 def call(String url = 'git@github.com:zhengzhixing/game-of-life.git') {
+    echo ${url}
     checkout([$class: 'GitSCM',branches: [[name: '* / master']],userRemoteConfigs: [[credentialsId: '4773d545-8a70-4245-bb89-d576be82414f', url: "${url}"]]])
 }
 
