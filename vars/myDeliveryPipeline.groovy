b85713edb8d98612368229c6ce9875b23338c5a3
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-06 11:03:04 +0800

    Create myDeliveryPipeline.groovy

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,31 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    parallel (
+                        "unit tests": { sh 'mvn test' },
+                        "integration tests": { sh 'mvn integration-test' }
+                    )
+                }
+            }
+            stage('Example') {
+                 steps {
+                     echo "build,test,succeed"
+                  }
+            }
+    }
+}
