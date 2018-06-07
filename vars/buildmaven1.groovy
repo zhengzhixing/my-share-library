6f109b4035547a68b4ae7d0fda5801e8f78e023a
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-07 14:24:23 +0800

    Update buildmaven1.groovy

--- vars/buildmaven1.groovy
+++ vars/buildmaven1.groovy
@@ -1,9 +1,11 @@
 def call(Map parameters) {
-pipeline {
-    agent any
+     environment { 
         def project = parameters.name
         def config = readYaml text: libraryResource('org/zzx/project.yml')
         def git_url = config.projects."$project".git_url
+      }
+pipeline {
+    agent any
     stages {
         stage('构建'){
              steps {
