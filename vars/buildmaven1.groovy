80416a14d1124b1ade0e3caf2d5f35f039651d6e
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-07 15:32:46 +0800

    Update buildmaven1.groovy

--- vars/buildmaven1.groovy
+++ vars/buildmaven1.groovy
@@ -1,33 +1,33 @@
 def call(Map parameters = [:]) {
-pipeline {
-    agent any
-environment{
-     def project = parameters.project
-     def config = readYaml text: libraryResource('org/zzx/project.yml')
-     def git_url = config.projects."$project".git_url
-}
-     stages {
-        stage('构建'){
-             steps {
-                  echo "$project"
-                  echo "$git_url"
-                 git url: "$git_url"
-                 sh "mvn install"
-             }
+    pipeline {
+        agent any
+        environment{
+            def project = parameters.project
+            def config = readYaml text: libraryResource('org/zzx/project.yml')
+            def git_url = config.projects."$project".git_url
         }
-        stage('测试'){
-             steps {
-                 echo "正在测试"
-                 sh 'mvn test'
-             }
-        }
-        stage('发布'){
-            steps {
-                echo "正在发布"
-                sh 'sh /var/lib/jenkins/tomcat-jdk.sh'
+        stages {
+            stage('构建'){
+                steps {
+                    echo "$project"
+                    echo "$git_url"
+                    git url: "$git_url"
+                    sh "mvn install"
+                }
             }
-        }
+            stage('测试'){
+                steps {
+                    echo "正在测试"
+                    sh 'mvn test'
+                }
+            }
+            stage('发布'){
+                steps {
+                    echo "正在发布"
+                    sh 'sh /var/lib/jenkins/tomcat-jdk.sh'
+                }
+            }
     
+        }
     }
-}
 }
