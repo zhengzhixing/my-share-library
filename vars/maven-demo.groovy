bdddadc6a6520af791dfee56943e0b4a4fce8470
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-07 14:09:04 +0800

    Create maven-demo.groovy

---
+++ vars/maven-demo.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    environment { 
+        def project = parameters.project
+        def config = readYaml text: libraryResource('org/zzx/project.yml')
+        def git_url = config.projects."$project".git_url
+    }
+    stages {
+        stage('构建'){
+             steps {
+                 print "${git_url}"
+             }
+        }
+        stage('测试'){
+             steps {
+                 echo "正在测试"
+                 sh 'mvn test'
+             }
+        }
+        stage('发布'){
+            steps {
+                echo "正在发布"
+                sh 'sh /var/lib/jenkins/tomcat-jdk.sh'
+            }
+        }
+    
+    }
+}
