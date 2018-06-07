019563325dae2cba1d907d256219b713526bc335
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-07 16:53:13 +0800

    Update buildmaven1.groovy

--- vars/buildmaven1.groovy
+++ vars/buildmaven1.groovy
@@ -9,7 +9,7 @@ def call(Map parameters = [:]) {
         stages {
             stage('Build'){
                 steps {
-                    git url: "git@github.com:zhengzhixing/${git_url}", branch: 'master'
+                    git url: "git@github.com:zhengzhixing${git_url}"
                     sh "mvn install"
                 }
             }
