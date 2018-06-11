da37b972f437c5a47e8aedf66bdce167d2595f4c
zhengzhixing <39557214+zhengzhixing@users.noreply.github.com>
2018-06-11 11:52:22 +0800

    Update buildmaven.groovy

--- vars/buildmaven.groovy
+++ vars/buildmaven.groovy
@@ -6,7 +6,7 @@ pipeline {
                   steps{
                       echo "${parameters.name}正在构建1"
                       git url: "git@github.com:zhengzhixing/${parameters.name}.git"
-                       sh "mvn install"
+                       sh "mv11n insta11ll"
                   }
               }
               stage('Test'){
